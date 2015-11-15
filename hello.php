6c63509a7c84068fb6efbcae86a06f22abc5e5e2
indridwiayu <iendyify@gmail.com>
2015-11-15 12:57:20 +0700

    Ubah index.php & tambah hello.php

---
+++ hello.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>Web App Sederhana</title>
+</head>
+	<?php
+	if(!empty($_POST)){
+		$nama = $_POST['nama'];
+		$email = $_POST['email'];
+
+		echo '<h1>Halo, ' . $nama . '!</h1>';
+		echo 'Email : ' . $email;
+	}
+	?>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-echo "Hello, Azure!";
->?
+<html>
+<head>
+	<title>Web App Sederhana</title>
+</head>
+	<form action="hello.php" method="POST">
+		Nama : <input type="text" name="nama"/>
+		<br>
+		Email : <input type="email" name="email"/>
+		<br>
+		<input type="submit" value="Enter"/>
+	</form>
+</html>
